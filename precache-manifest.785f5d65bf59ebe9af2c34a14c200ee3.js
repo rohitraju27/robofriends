ed104a72dae6a796cc7a7d70835689a3ea81088e
unknown <rohu270700@gmail.com>
2020-07-10 18:42:31 +0530

    Updates

---
+++ precache-manifest.785f5d65bf59ebe9af2c34a14c200ee3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df241b9c4c351236c9bcec7d27d362b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0d44ed1cbca25898a8be",
+    "url": "/static/css/main.d7fd1605.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d44ed1cbca25898a8be",
+    "url": "/static/js/main.0850a547.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
